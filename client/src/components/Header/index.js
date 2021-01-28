43b50a470a59aedf9b6f3eff80bd2f200270ced2
Matthew Knight <mattknight.sa@gmail.com>
2021-01-28 17:45:41 -0600

    creating header

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './style.css';
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>Matt Knight Portfolio</h1>
+        </div>
+    )
+}
+
+export default Header;
