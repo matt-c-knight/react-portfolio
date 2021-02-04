a437ba5ae5e59ca855224be32dfe226ae0fe962e
Matthew Knight <mattknight.sa@gmail.com>
2021-02-03 23:57:07 -0600

    adding image

--- client/src/components/AboutMe/index.js
+++ client/src/components/AboutMe/index.js
@@ -7,11 +7,12 @@ function AboutMe(props) {
     return (
         <div className="wrapper">
             <h1 className='head'>About Me</h1>
-            <img src={profile}/>
+            <img src={profile} className="photo" />
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
         </div>
 
 
-    );
+    ); 
 }
 
 export default AboutMe;
